b7caa2c234b2fd631b0d8051189ecab768ab34f3
Sav Younts <savyounts@gmail.com>
2018-08-23 18:24:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,27 +43,36 @@ class Student
     def self.create (name, grade)
       student = self.new (name, grade)
       student.save
-      student 
+      student
     end
 
     def self.new_from_db(row)
-      student = self.new 
+      student = self.new
       student.id = row[0]
       student.name = row[1]
       student.grade = row[2]
-      student 
-    end 
+      student
+    end
 
     def self.find_by_name(name)
-      sql = <<-SQL 
+      sql = <<-SQL
       SELECT * FROM students
       WHERE students.name = ?
-      SQL 
+      SQL
 
       DB[:conn].execute(sql, name).map do |row|
         self.new_from_db(row)
       end
     end
+
+    def update 
+      sql = <<-SQL 
+      UPDATE students SET students.name = ?, students.grade = ?
+      WHERE students.id = ?
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade, self.id)
+    end 
   end
 
 
