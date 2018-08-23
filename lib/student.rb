54ee75ff8c576095c0daeea628a381a2f016150d
Sav Younts <savyounts@gmail.com>
2018-08-23 19:46:56 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,8 +39,8 @@ class Student
   end
   end
 
-    def self.create
-      student = self.new
+    def self.create(name, grade)
+      student = self.new(name, grade)
       student.save
       student
     end
